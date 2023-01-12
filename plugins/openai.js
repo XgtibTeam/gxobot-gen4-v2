57fb409d3e4238643b93c8a7d5eae19dc28bfa82
XgtibxTeam <110323408+XgtibTeam@users.noreply.github.com>
2023-01-12 14:18:29 +0700

    Add files via upload

--- plugins/openai.js
+++ plugins/openai.js
@@ -1,9 +1,21 @@
-import fetch from 'node-fetch'
+let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command, args, usedPrefix, text }) => {
-    if (!args[0]) return false
-    let res = await (await fetch(`http://tools-amfcode.com/api/ai/bot.php?text=${text}`)).json()
+    if (!args[0]){ m.reply( 'Masukan teks')
+    return false
+}
+try{
+let res = await (await fetch(`https://malesin.xyz/ai?text=${text}&apikey=DonasiDong`)).json()
+await conn.reply(m.chat, `*CHAT OPENAI*\n\n${res.message}`)
+}catch(e) {
+m.reply('eror!')
+}
+/*
+let res = await (await fetch(`http://tools-amfcode.com/api/ai/bot.php?text=${text}`)).json()
     await conn.reply(m.chat, `*CHAT OPENAI*${res.text}`, m)
+}catch(e){
+m.reply('maaf fitur sedang eror silahkan coba kembali nanti!')
+}*/
 }
 handler.help = ['ai']
 handler.tags = ['tools']
@@ -11,5 +23,6 @@ handler.tags = ['tools']
 handler.command = /^ai$/i
 handler.limit = true
 handler.group = false
+handler.premium = true
 
-export default handler
+module.exports = handler
